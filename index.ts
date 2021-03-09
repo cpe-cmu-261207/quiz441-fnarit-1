58cc952c47f375850578bc603690946a1b65a767
fnarit <naritsararat@gmail.com>
2021-03-09 16:14:54 +0700

    1

--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ app.use(cors())
 
 const PORT = process.env.PORT || 3000
 const SECRET = "SIMPLE_SECRET"
+const user = require('../models/user')
 
 interface JWTPayload {
   username: string;
@@ -21,11 +22,18 @@ app.post('/login',
   (req, res) => {
 
     const { username, password } = req.body
-    // Use username and password to create token.
-
-    return res.status(200).json({
-      message: 'Login succesfully',
-    })
+    //Use username and password to create token.
+    if(!username || req.body.password !== user.password){
+      return res.status(400).json({
+        message: 'Invalid username or password',
+      })
+    }else{
+      return res.status(200).json({
+        message: 'Login succesfully',
+        token: 'token ที่ระบบสร้างขึ้น'
+      })}
+      const token = jwt.sign({username :user.username},SECRET)
+      res.json({token})
   })
 
 app.post('/register',
@@ -39,25 +47,35 @@ app.get('/balance',
     const token = req.query.token as string
     try {
       const { username } = jwt.verify(token, SECRET) as JWTPayload
-  
+      
     }
     catch (e) {
       //response in case of invalid token
+      res.status(401).json({
+        message: 'Invalid token'
+      })
     }
   })
 
 app.post('/deposit',
   body('amount').isInt({ min: 1 }),
   (req, res) => {
-
+    
     //Is amount <= 0 ?
     if (!validationResult(req).isEmpty())
+    if(user.amount <= 0)
       return res.status(400).json({ message: "Invalid data" })
+    else if(user.amount >0) return res.status(401).json({ message: "Deposit token"})
+    else return res.status(200).json({ message: "Deposit successfully", balance:"200" })
   })
 
 app.post('/withdraw',
   (req, res) => {
-  })
+
+    if (!validationResult(req).isEmpty())
+    return res.status(400).json({ message: "Invalid data" })
+})
+  
 
 app.delete('/reset', (req, res) => {
 
